274c10fe80056073f7ea234d6e6c2482dafd3203
Unsouled <unsouled0@gmail.com>
2011-06-09 17:43:32 +0900

    pilot code

--- test/apns.test.js
+++ test/apns.test.js
@@ -3,14 +3,58 @@ var nodeunit = require('nodeunit');
 
 module.exports = nodeunit.testCase({
     setUp: function(callback) {
+        invalidToken = 'gfeaws';
+        validNotRegisteredToken = '00000000';
+        validRegisteredToken = '11111111';
         callback();
     },
     tearDown: function(callback) {
         callback();
     },
     testValidPush: function(test) {
+        var validOptions = {
+            deviceToken: validRegisteredToken
+        };
+        apns.push(validOptions, function() {
+            test.done();
+        });
+    },
+    testValidEnhancedPush: function(test) {
         var validOptions = {};
-        apns.push(options, function() {
+        apns.enhancedPush(validOptions, function() {
+            test.done();
+        });
+    },
+    testInvalidTokenEnhancedPush: function(test) {
+        var invalidOptions = {};
+        var request = apns.enhancedPush(invalidOptions, function() {});
+        request.on('error', function(status, message) {
+            test.equals(apns.errorCode.INVALID_TOKEN, status);
+            test.done();
+        });
+    },
+    testMissingPayloadEnhancedPush: function(test) {
+        var invalidOptions = {};
+        var request = apns.enhancedPush(invalidOptions, function() {});
+        request = apns.on('error', function(status, message) {
+            test.equals(apns.errorCode.MISSING_PAYLOAD, status);
+            test.done();
+        });
+    },
+    testInvalidTokenSizeEnhancedPush: function(test) {
+        var invalidOptions = {};
+        var request = apns.enhancedPush(invalidOptions, function() {});
+        request = apns.on('error', function(status, message) {
+            test.equals(apns.errorCode.INVALID_TOKEN_SIZE, status);
+            test.done();
+        });
+
+    },
+    testInvalidPayloadSizeEnhancedPush: function(test) {
+        var invalidOptions = {};
+        var request = apns.enhancedPush(invalidOptions, function() {});
+        request.on('error', function(status, message) {
+            test.equals(apns.errorCode.INVALID_PAYLOAD_SIZE, status);
             test.done();
         });
     },
